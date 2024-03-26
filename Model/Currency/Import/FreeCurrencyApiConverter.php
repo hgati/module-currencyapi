fa243309c11fcd44e7d878cf0722b6159aa69c3b
Dennis <cobays@gmail.com>
2024-03-26 09:25:28 +0900

    update

--- Model/Currency/Import/FreeCurrencyApiConverter.php
+++ Model/Currency/Import/FreeCurrencyApiConverter.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Copyright © Dennis Lee All rights reserved.
- * See COPYING.txt for license details.
- */
 declare(strict_types=1);
 
 namespace Hgati\CurrencyConverter\Model\Currency\Import;
